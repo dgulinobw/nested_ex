2c75376dc063f0f8be4da9df7a5a5493a3861000
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2023-08-22 17:19:48 -0400

    published to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,12 +1,19 @@
 defmodule Nested.MixProject do
   use Mix.Project
 
+  @name "nested_ex"
+  @version "0.1.0"
+  @repo_url "https://github.com/dgulinobw/nested_ex"
+
   def project do
     [
       app: :nested_ex,
-      version: "0.1.0",
+      version: @version,
+      description: "Library to handle nested maps",
+      package: package(),
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      name: @name,
       deps: deps()
     ]
   end
@@ -21,8 +28,16 @@ defmodule Nested.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @repo_url,
+      }
     ]
   end
 end
